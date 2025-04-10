acd307d0a701cb8b97f2b0b28da34b2d58e6e2af
Kavya <kavyakrishna@zktecodev.com>
2025-04-10 13:00:22 +0530

    Initial commit

---
+++ EMPM/EMPMTests/EMPMTests.swift
@@ -0,0 +1,17 @@
+//
+//  EMPMTests.swift
+//  EMPMTests
+//
+//  Created by Kavya Krishna K. on 10/04/25.
+//
+
+import Testing
+@testable import EMPM
+
+struct EMPMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
