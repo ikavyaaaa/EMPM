87d166e0dad5e258642ff097d016afef138f4d05
Kavya <kavyakrishna@zktecodev.com>
2025-04-10 14:10:42 +0530

    Tabbar added

---
+++ EMPM/EMPM/Supporting/Tabbar/TabBarView.swift
@@ -0,0 +1,140 @@
+//
+//  TabBarView1.swift
+//  EMPM
+//
+//  Created by Kavya Krishna K. on 10/04/25.
+//
+
+
+import SwiftUI
+
+struct TabBarView: View {
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(.white)
+                .shadow(color: .gray.opacity(0.4), radius: 20, x: 0, y: 20)
+            
+            TabsLayoutView()
+        }
+        .frame(height: 70, alignment: .center)
+    }
+}
+
+fileprivate struct TabsLayoutView: View {
+    @State var selectedTab: Tab = .home
+    @Namespace var namespace
+    
+    var body: some View {
+        HStack {
+            Spacer(minLength: 0)
+            
+            ForEach(Tab.allCases) { tab in
+                TabButton(tab: tab, selectedTab: $selectedTab, namespace: namespace)
+                    .frame(width: 65, height: 65, alignment: .center)
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+    
+    
+    
+    private struct TabButton: View {
+        let tab: Tab
+        @Binding var selectedTab: Tab
+        var namespace: Namespace.ID
+        
+        var body: some View {
+            Button {
+                withAnimation {
+                    selectedTab = tab
+                }
+            } label: {
+                ZStack {
+                    if isSelected {
+                        Circle()
+                            .shadow(radius: 10)
+                            .background {
+                                Circle()
+                                    .stroke(lineWidth: 15)
+                                    .foregroundColor(.clear)
+                            }
+                            .offset(y: -40)
+                            .matchedGeometryEffect(id: "Selected Tab", in: namespace)
+                            .animation(.spring(), value: selectedTab)
+                    }
+                    
+                    Image(systemName: tab.icon)
+                        .font(.system(size: 23, weight: .semibold, design: .rounded))
+                        .foregroundColor(isSelected ? .init(white: 0.9) : .gray)
+                        .scaleEffect(isSelected ? 1 : 0.8)
+                        .offset(y: isSelected ? -40 : 0)
+                        .animation(isSelected ? .spring(response: 0.5, dampingFraction: 0.3, blendDuration: 1) : .spring(), value: selectedTab)
+                }
+            }
+            .buttonStyle(.plain)
+        }
+        
+        private var isSelected: Bool {
+            selectedTab == tab
+        }
+    }
+}
+
+enum Tab: Int, Identifiable, CaseIterable, Comparable {
+    static func < (lhs: Tab, rhs: Tab) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+    
+    case home, game, apps, movie
+    
+    internal var id: Int { rawValue }
+    
+    var icon: String {
+        switch self {
+        case .home:
+            return "house.fill"
+        case .game:
+            return "gamecontroller.fill"
+        case .apps:
+            return "square.stack.3d.up.fill"
+        case .movie:
+            return "play.tv.fill"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .home:
+            return "Home"
+        case .game:
+            return "Employees"
+        case .apps:
+            return "Video"
+        case .movie:
+            return "More"
+        }
+    }
+    
+    var color: Color {
+        switch self {
+        case .home:
+            return .indigo
+        case .game:
+            return .pink
+        case .apps:
+            return .orange
+        case .movie:
+            return .teal
+        }
+    }
+}
+
+struct TabBarView1_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+            .padding(.bottom, 10)
+    }
+}
